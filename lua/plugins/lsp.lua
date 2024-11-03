5edf7e511b273e55689f0233d08a67d6df358e0b
Mahmud Youssef <mahmud.youssef@icloud.com>
2024-11-03 03:12:30 +0200

    updates on LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,45 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-        },
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ruff", "rust_analyzer" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "antosha417/nvim-lsp-file-operations"
-        },
-        config = function()
-            local lspConfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ruff", "rust_analyzer" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"antosha417/nvim-lsp-file-operations",
+		},
+		config = function()
+			local lspConfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- start up file operation support
-            require("lsp-file-operations").setup()
+			-- start up file operation support
+			require("lsp-file-operations").setup()
 
-            -- Lua lsp configuration
-            lspConfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			-- Lua lsp configuration
+			lspConfig.lua_ls.setup({
+				filetypes = { "lua" },
+				capabilities = capabilities,
+			})
 
-            -- Python lsp configuration
-            lspConfig.ruff.setup({
-                capabilities = capabilities,
-            })
+			-- Python lsp configuration
+			lspConfig.ruff.setup({
+				filetypes = { "python" },
+				capabilities = capabilities,
+			})
 
-            -- Rust lsp configuration
-            lspConfig.rust_analyzer.setup({
-                capabilities = capabilities,
-            })
+			-- Rust lsp configuration
+			lspConfig.rust_analyzer.setup({
+				filetypes = { "rust" },
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
