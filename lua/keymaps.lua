cce38a5dfc99ed428e187590933a2b6f8b14fe2c
Mahmud Youssef <mahmud.youssef@icloud.com>
2024-11-05 23:13:02 +0200

    fix tabs issue

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,9 +8,13 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- file saving
-vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts) -- save file
-vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts) -- without auto-formatting
-vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts) -- quit file
+vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
+
+-- without auto-formatting
+vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
+
+-- quit file
+vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
 
 -- delete single character without copying into register
 vim.keymap.set("n", "x", '"_x', opts)
