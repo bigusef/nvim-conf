5aa405aa889e3d4e932ae8cd6fada84f5efe41cf
Mahmud Youssef <mahmud.youssef@icloud.com>
2024-11-05 10:45:26 +0200

    updates on basic configuration

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,12 @@
 -- For conciseness
 local opts = { noremap = true, silent = true }
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
 -- file saving
 vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts) -- save file
 vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts) -- without auto-formatting
--- lua/options.lua
+++ lua/options.lua
@@ -1,16 +1,30 @@
 -- set the leaders keys
 vim.g.mapleader = " "
-vim.g.maplocalleader = ","
+vim.g.maplocalleader = " "
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
 
 -- enable line numbers
 vim.wo.number = true
 vim.o.relativenumber = true
 
--- enable copy from vim to system clipbord
-vim.o.clipboard = "unnamedplus"
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
 
 -- enhance color's showing in the Termnail
 vim.o.termguicolors = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
 
 -- windows split
 vim.o.splitright = true
@@ -19,6 +33,20 @@ vim.o.splitbelow = true
 -- enable case insensitive UNLESS \C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'` and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- indentation options
 vim.o.shiftwidth = 4
